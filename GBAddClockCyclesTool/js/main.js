7a3e0044783ba92f57483401a412c72e6bc8e469
emutyworks <emuty@emutyworks.com>
2023-08-20 09:53:27 +0900

    Update it

--- GBAddClockCyclesTool/js/main.js
+++ GBAddClockCyclesTool/js/main.js
@@ -1,3 +1,5 @@
+//window.onload = function(){ add_comment(); } //debug
+
 function add_comment(){
   var asm_txt = '';
   var asm_array = null;
@@ -10,38 +12,27 @@ function add_comment(){
     var row_org = asm_array[i];
     var row = row_org.replace(/\t/g,'').toLowerCase().trim();
 
-    var row_left = row.split(' ');
+    var row_nocomment = row.split(';');
+    var row_left = row_nocomment[0].trim().split(' ');
     var key = '';
 
     if(instruction.indexOf(row_left[0]) != -1){
       if(row_left[1]!==undefined){
-        var row_right = row_left[1].split(',');
+        var row_right = row_left[1].trim().split(',');
         var row_right1 = row_right[0];
         var row_right2 = row_right[1];
 
         if(row_right1.indexOf('[') != -1){
-          row_right1 = replacement[row_right1.trim()];
-          if(row_right1===undefined){
-            row_right1 = '[nn]';
-          }
+          row_right1 = checkRow(row_right1,'[nn]');
         }else{
-          row_right1 = replacement[row_right1.trim()];
-          if(row_right1===undefined){
-            row_right1 = 'n';
-          }
+          row_right1 = checkRow(row_right1,'n');
         }
 
         if(row_right2!==undefined){
           if(row_right2.indexOf('[') != -1){
-            row_right2 = replacement[row_right2.trim()];
-            if(row_right2===undefined){
-              row_right2 = '[nn]';
-            }
+            row_right2 = checkRow(row_right2,'[nn]');
           }else{
-            row_right2 = replacement[row_right2.trim()];
-            if(row_right2===undefined){
-              row_right2 = 'n';
-            }
+            row_right2 = checkRow(row_right2,'n');
           }
         }
 
@@ -55,7 +46,18 @@ function add_comment(){
       }
     }
     if(opcode[key]){
-      result += row_org+' ;'+opcode[key]+"\n";
+      var clock = ';'+opcode[key];
+      if(row_org.indexOf(clock+" ") == -1 && (row_org.indexOf(clock) == -1
+      || row_org.indexOf(clock)!=(row_org.length - clock.length))
+      ){
+        if(row_org.indexOf(';') != -1){
+          result += row_org.replace(';', clock+' ;')+"\n";
+        }else{
+          result += row_org+' '+clock+"\n";
+        }
+      }else{
+        result += row_org+"\n";
+      }
     }else
     if(key==''){
       result += row_org+"\n";
@@ -67,4 +69,12 @@ function add_comment(){
     }
   }
   $("textarea[name='result']").text(result);
+}
+
+function checkRow(row,val){
+  row = replacement[row.trim()];
+  if(row===undefined){
+    row = val;
+  }
+  return row;
 }
